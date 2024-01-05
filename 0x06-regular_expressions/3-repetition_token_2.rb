cb39a48960dbb7ccd551aaac430e7d0369029f10
Promgirl65 <olayinkabusolami@gmail.com>
2024-01-05 04:30:42 -0800

    0x06-regular_expressions

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Ruby script for Repetition Token #2
+
+puts ARGV[0].scan(/hbt+n/).join
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Ruby scripy for Repetition Token #3
+
+puts ARGV[0].scan(/hbt*n/).join
